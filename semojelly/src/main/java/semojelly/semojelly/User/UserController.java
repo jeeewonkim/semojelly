fba230baae08bbd7d5a854159751cbde34403f28
jiwonKim <78153919+jeeewonkim@users.noreply.github.com>
2022-07-08 12:34:12 +0900

    feat: 로그인/ 회원가입 코드

--- semojelly/src/main/java/semojelly/semojelly/User/UserController.java
+++ semojelly/src/main/java/semojelly/semojelly/User/UserController.java
@@ -5,14 +5,11 @@ import lombok.extern.slf4j.Slf4j;
 import org.apache.coyote.Response;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @Slf4j
 @RestController
-@RequestMapping("user")
+@RequestMapping(value="user",method = {RequestMethod.GET, RequestMethod.POST})
 @RequiredArgsConstructor
 public class UserController {
 
